89e17993f281a327a41e048f0e187561577fe6f1
marirs-dev <marirs.dev@gmail.com>
2024-03-04 23:13:21 +0530

    vite and package updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,36 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+        '@': fileURLToPath(new URL('./src/components/Table.vue', import.meta.url)),
+    },
+},
+server: {
+  host: true,
+  port: 8080,
+},
   build: {
+    emptyOutDir: false,
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es", "cjs"],
-      fileName: (format) => `vue-table.${format}.js`
+      entry: resolve(__dirname, "src", "index.ts"),
+      name: 'VueTable',
+      formats: ['es', 'umd'],
+      fileName: 'pangea-vue-table'
     },
     rollupOptions: {
-      external: ["vue"],
+      external:['vue'],
       output: {
-        preserveModules: true,
-        exports: "named"
-      }
-    }
+          globals: {
+              vue: 'Vue',
+              'date-fns': 'dateFns',
+          },
+      },
+  },
   }
 });
